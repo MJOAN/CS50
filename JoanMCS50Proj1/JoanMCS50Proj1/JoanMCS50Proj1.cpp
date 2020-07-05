cc4a09466c2fbd63d5e425bb02030feaa46e1514
Mariam Joan <royalton@gmail.com>
2020-07-05 07:12:44 -0700

    week 1 project

---
+++ JoanMCS50Proj1/JoanMCS50Proj1/JoanMCS50Proj1.cpp
@@ -0,0 +1,23 @@
+//
+// main.cpp
+//
+// Mariam Joan
+// Project 1
+// Program to print name and course information
+// Due: June 28, 2020
+// Copyright © 2020 Mariam Joan. All rights reserved.
+
+#include <iostream>
+
+int main()
+{
+    // the following are prints statements with line breaks
+    
+    printf("Mariam Joan - Project 1");
+    printf("\n\n");
+    printf("Santa Monica College\n");
+    printf("Computer Science Information Systems\n");
+    printf("CS 50: C Programming");
+    printf("\n\n");
+    printf("Mariam Joan - end of Project 1\n");
+}
